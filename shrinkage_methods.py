8e6e6a0ade13ad9f03759faafbcd2b9d6709cbe8
Marco Feder <marco.feder96@gmail.com>
2019-07-22 20:13:06 +0200

    Shrinkage methods (Ridge, Lasso)

---
+++ shrinkage_methods.py
@@ -0,0 +1,83 @@
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+from sklearn import linear_model
+from sklearn.linear_model import LinearRegression, Ridge
+from sklearn.metrics import mean_squared_error
+
+data=pd.read_csv('prostate.csv',sep=',',engine='python',index_col='train')
+
+#generazione del train set: devo identificare le righe che corrispondono a train=T
+dataTrain=data.loc['T'] #estrae le righe che corrispondono al train set, prendendole dal data frame.
+#print('La dimensione del train set è: \n',data.shape)
+
+#Ora rimuovo ciò che non serve, ossia la colonna lpsa, che è ciò che devo utilizzare come y nella regressione, e la salvo.
+lpsaTrain=dataTrain.iloc[:,-1] #prendo l'ultimo
+dataTrain=dataTrain.drop('lpsa',axis=1)
+predictorTrain=dataTrain
+
+#Genero il test set.
+dataTest=data.loc['F']
+lpsaTest=dataTest.iloc[:,-1]
+dataTest=dataTest.drop('lpsa',axis=1)
+
+
+#standardizzare le variabili
+#1. calcolo la media per ogni colonna
+predictorTrainMean=np.zeros([1,np.array(dataTrain.shape)[1]])
+for i in range (0,np.array(dataTrain.shape)[1]):
+    predictorTrainMean[0,i]=predictorTrain.iloc[:,i].mean()
+
+#2. calcolo standard deviation
+predictorTrainStd=np.zeros([1,np.array(dataTrain.shape)[1]])
+for i in range (0,np.array(dataTrain.shape)[1]):
+    predictorTrainStd[0,i]=predictorTrain.iloc[:,i].std()
+
+#3. standardizing variables
+
+for i in range (0,np.array(dataTrain.shape)[1]):
+    predictorTrain.iloc[:,i]=(predictorTrain.iloc[:,i]-predictorTrainMean[0,i])/(predictorTrainStd[0,i])
+
+predictorTrain_std=predictorTrain #assegno il train set standardizzato a una nuova variaibile
+
+
+#generate linear regression model on train set
+reg=linear_model.Ridge(alpha=1.0)
+reg.fit(predictorTrain_std,lpsaTrain)
+
+print('Ridge regression coefficients are:', reg.coef_)
+print('Ridge regression intercept is:', reg.intercept_)
+print('Ridge regression R^2 is:', reg.score(predictorTrain_std,lpsaTrain))
+
+#plot Ridge coefficients as a function of the regularization parameters
+
+alphas=np.logspace(-1,4,200) #vector of regularization parameters
+coefs=list()
+for i in range(len(alphas)):
+    reg=linear_model.Ridge(alpha=alphas[i])
+    reg.fit(predictorTrain_std,lpsaTrain)
+    coefs.append(reg.coef_)
+
+#plot alphas against the related coefficients: cfr http://www.ds100.org/sp19/assets/lectures/regularization.pdf#Navigation24
+plt.plot(alphas,coefs)
+plt.xlabel('regularization parameter')
+plt.ylabel('coefficients')
+#plt.legend('lcavol','lweight','age','lbph','svi','lcp','gleason','pgg45') save coeff in a matrix to plot also the legend
+plt.title('Regularization parameter against Ridge coeffs')
+plt.show()
+
+
+#find the best regularization parameter by cross-validation
+reg = linear_model.RidgeCV(alphas=np.logspace(-1,4,200),store_cv_values=True)
+reg.fit(predictorTrain_std,lpsaTrain)
+print(reg.cv_values_.shape)
+
+#Compute the mean by column of cv_values_: I obtain the average cross-validation error for each alpha. This is the value that has to be minimized 
+colMean=np.zeros([reg.cv_values_.shape[1],1])
+for i in range(reg.cv_values_.shape[1]):
+    colMean[i]=reg.cv_values_[:,i].mean()
+
+#Plot the leave-one-out cross validation error
+plt.plot(alphas,colMean)
+plt.show()
