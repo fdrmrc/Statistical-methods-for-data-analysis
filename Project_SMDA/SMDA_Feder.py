cb0f9c06b318b36f83848d2491f98b9a84eb810b
Marco Feder <marco.feder96@gmail.com>
2019-07-25 13:51:05 +0200

    add corrlation matrix

--- Project_SMDA/SMDA_Feder.py
+++ Project_SMDA/SMDA_Feder.py
@@ -19,6 +19,56 @@ data['Date']=split[0] #assign the splitted variables to two new columns
 data['Hours']=split[1]
 data=data.drop('Date_1',axis=1) #drop old columns
 
-print(data.head())
 
-#Build the variable Weekday. The sintax is >> date(2019,7,25).isoweekday(), so I need yo cut off the "-" in each date
+#Build the variable Weekday. The sintax is >> date(ANNO,MESE,GIORNO).isoweekday(), so I need yo cut off the "-" in each date by using split('-')
+years= []
+months= []
+days= []
+
+weekdays=[] #--> it will be the column Weekday
+hours = [] #-->  it will be the column Hours
+weekends=[] #--> it will be the column Weekends
+for i in range (0,data.shape[0]):
+    splitDate=data['Date'][i].split('-')
+    splitHour=data['Hours'][i].split(':')
+    years.append(splitDate[0])
+    months.append(splitDate[1])
+    days.append(splitDate[2])
+    weekdays.append(date(int(years[-1]),int(months[-1]),int(days[-1])).isoweekday()) #every time I compute the date by looking at the last element in years
+    if (weekdays[-1]== 6 or weekdays[-1]==7):
+        weekends.append(1)
+    else:
+        weekends.append(0)
+  
+    hours.append(int(splitHour[0]))
+
+#Now I add the columns to the dataframe. In order to do that, I have to create Series (which are columns in a DataFrame)
+Weekday=pd.Series(weekdays).values
+data['Weekday']=Weekday
+
+Weekend=pd.Series(weekends).values
+data['Weekends']=Weekend
+
+Hour=pd.Series(hours).values
+data['Hour']=Hour
+
+#Add columns for year and month and day
+
+Day=pd.Series(days).values
+data['Day']=Day
+
+Year=pd.Series(years).values
+data['Year']=Year
+
+Month=pd.Series(months).values
+data['Month']=Month
+
+data=data.drop('Date',axis=1)
+data=data.drop('Hours',axis=1)
+print(data.head())
+print(data.tail())
+
+#Plot of the correlation matrix
+M=data.corr()
+plt.matshow(M)
+plt.show()  # better to add some labels 
